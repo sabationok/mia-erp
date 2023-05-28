0b9a800ad9d7bb530a2dc3ec1ca817ef3b80f7a6
goodcat46 <110983329+sabationok@users.noreply.github.com>
2023-05-29 00:30:55 +0300

    74 AppPages fix

--- src/components/AppRoutes/AppRoutes.tsx
+++ src/components/AppRoutes/AppRoutes.tsx
@@ -1,49 +1,34 @@
-import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import AppPages from 'components/AppPages';
 import { memo, useMemo } from 'react';
 import PublicRoute from './PublicRoute';
 import PrivateRoute from './PrivateRoute';
-import FlexBox from '../atoms/FlexBox';
+import { useAuthSelector } from '../../redux/selectors.store';
 
 const { PageNotFound } = AppPages;
 
 const AppRoutes: React.FC = () => {
-  const location = useLocation();
-  
+  const { accessToken } = useAuthSelector();
+
+  const isAuthorized = useMemo(() => !!accessToken, [accessToken]);
+
   const notFoundRouteProps = useMemo(
     () => ({
       path: '*',
       element: <PageNotFound />,
-      errorElement: <PageNotFound />,
     }),
     []
   );
 
   return (
     <Routes>
-      <Route path={`/*`} element={<Navigate to={`/auth`} />} errorElement={<PageNotFound />} />
+      <Route index element={<Navigate to={'/auth'} />} />
 
-      <Route path={`/*`} element={<Navigate to={`${location.pathname}`} />} />
+      {/*<Route path={'/'} element={<Navigate to={'/auth'} />} />*/}
 
-      <Route
-        path={'/'}
-        element={
-          <FlexBox
-            fillWidth
-            fillHeight
-            style={{ fontSize: '24px', fontWeight: 900 }}
-            padding={'50px'}
-            alignItems={'center'}
-            justifyContent={'center'}
-          >
-            Index test page for github
-          </FlexBox>
-        }
-      />
-
-      <Route path={`/*`} element={<PrivateRoute redirectTo="/auth" />}>
+      <Route path={'*'} element={<PrivateRoute redirectTo="/auth" />}>
         <Route path="/*" element={<AppPages.AppGridPage path={'transactions'} />} errorElement={<PageNotFound />}>
-          <Route index element={<Navigate to={'transactions'} />} errorElement={<PageNotFound />} />
+          <Route index element={<Navigate to={'/transactions'} />} errorElement={<PageNotFound />} />
 
           <Route path={'transactions'} element={<AppPages.PageTransactions />} errorElement={<PageNotFound />} />
           <Route path={'home'} element={<AppPages.PageHome />} errorElement={<PageNotFound />} />
@@ -60,8 +45,8 @@ const AppRoutes: React.FC = () => {
 
           <Route {...notFoundRouteProps} />
         </Route>
-        <Route {...notFoundRouteProps} />
       </Route>
+
       <Route {...notFoundRouteProps} />
     </Routes>
   );
