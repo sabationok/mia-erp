e49e2934c230c8c0f2880a2a53c9090829c36c30
goodcat46 <110983329+sabationok@users.noreply.github.com>
2024-05-19 18:46:18 +0300

    Update FormArea.tsx

--- src/components/Forms/FormArea/FormArea.tsx
+++ src/components/Forms/FormArea/FormArea.tsx
@@ -2,6 +2,10 @@ import FlexBox, { FlexForm } from '../../atoms/FlexBox';
 import { Text } from '../../atoms/Text';
 import { AppSubmitHandler } from '../../../hooks/useAppForm.hook';
 import FormAreaFooter from './FormAreaFooter';
+import { useEffect, useState } from 'react';
+import { isUndefined } from 'lodash';
+import ButtonIcon from '../../atoms/ButtonIcon/ButtonIcon';
+import styled from 'styled-components';
 
 export interface FormAreaProps {
   label?: string;
@@ -14,6 +18,8 @@ export interface FormAreaProps {
   renderTitle?: React.ReactNode;
   isLoading?: boolean;
   disabled?: boolean;
+  isOpen?: boolean;
+  expandable?: boolean;
 }
 
 export const FormArea = ({
@@ -26,24 +32,64 @@ export const FormArea = ({
   isLoading,
   onReset,
   hideLabel,
+  expandable = true,
+  isOpen = true,
 }: FormAreaProps) => {
+  const [_isOpen, _setIsOpen] = useState(isOpen ?? true);
+
+  const onToggleIsOpenHandler = () => {
+    _setIsOpen(p => !p);
+  };
+  useEffect(() => {
+    if (!isUndefined(isOpen)) {
+      _setIsOpen(isOpen);
+    }
+  }, [isOpen]);
+
   return (
-    <FlexForm fillWidth onSubmit={onSubmit} onReset={onReset}>
+    <FlexForm fillWidth onSubmit={onSubmit} onReset={onReset} style={{ position: 'relative' }}>
       {!hideLabel && (
-        <FlexBox padding={'8px 6px'} overflow={'hidden'}>
+        <Header
+          padding={'8px 6px'}
+          overflow={'hidden'}
+          fxDirection={'row'}
+          justifyContent={'space-between'}
+          alignItems={'center'}
+        >
           {renderTitle || (
             <Text $size={14} $weight={600}>
               {label}
             </Text>
           )}
-        </FlexBox>
+
+          <ButtonIcon
+            variant={'onlyIconNoEffects'}
+            icon={_isOpen ? 'SmallArrowUp' : 'SmallArrowDown'}
+            onClick={onToggleIsOpenHandler}
+          />
+        </Header>
       )}
 
-      {children}
+      <ExpandableBox isActive={_isOpen}>
+        {children}
 
-      {renderFooter || (
-        <FormAreaFooter hasOnSubmit={!!onSubmit} hasOnReset={!!onReset} isLoading={isLoading} disabled={disabled} />
-      )}
+        {renderFooter || (
+          <FormAreaFooter hasOnSubmit={!!onSubmit} hasOnReset={!!onReset} isLoading={isLoading} disabled={disabled} />
+        )}
+      </ExpandableBox>
     </FlexForm>
   );
 };
+const Header = styled(FlexBox)`
+  position: sticky;
+  top: 0;
+  left: 0;
+  z-index: 6;
+
+  background-color: ${p => p.theme.modalBackgroundColor};
+`;
+const ExpandableBox = styled(FlexBox)`
+  overflow: hidden;
+  max-height: ${p => (p.isActive ? '100%' : '0')};
+  //transition: all ${p => p.theme.globals.timingFunctionMain};
+`;
